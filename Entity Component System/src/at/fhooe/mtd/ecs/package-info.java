753a71ca04bd6e67b5a06a5b3d3ff2abfa91dace
Roman Divotkey <roman@divotkey.net>
2016-10-26 09:59:05 +0200

    Updated copyright header.

--- Entity Component System/src/at/fhooe/mtd/ecs/package-info.java
+++ Entity Component System/src/at/fhooe/mtd/ecs/package-info.java
@@ -1,3 +1,13 @@
+/*******************************************************************************
+ * Copyright (c) 2016 Roman Divotkey, Univ. of Applied Sciences Upper Austria. 
+ * All rights reserved.
+ *  
+ * This file is subject to the terms and conditions defined in file
+ * 'LICENSE', which is part of this source code package.
+ *  
+ * THIS CODE IS PROVIDED AS EDUCATIONAL MATERIAL AND NOT INTENDED TO ADDRESS
+ * ALL REAL WORLD PROBLEMS AND ISSUES IN DETAIL.
+ *******************************************************************************/
 /**
  * This package contains interfaces and classes to provide a simple but somewhat
  * complete component-based entity management system (ECS).
